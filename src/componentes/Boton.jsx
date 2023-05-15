67aeaf0ef0738278a0e150ff3b7e9bba5ce02286
axelpecode <axelpelassini@hotmail.com>
2023-05-15 11:41:50 -0300

    frp-commit09

--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Boton = ({nombreBoton, clickEfecto, idBoton, claseBoton}) => (
     <button id={idBoton} className={claseBoton} onClick={clickEfecto}>{nombreBoton}</button>
 );
